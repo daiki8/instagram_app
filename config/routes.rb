b49135d82625a11ac591c947d4bc78d0392b71c1
daiki8 <daiki1945815@gmail.com>
2018-09-28 17:48:48 +0900

    add signup-route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get '/signup', to: 'users#new'
+	post '/signup', to: 'users#create'
 end
