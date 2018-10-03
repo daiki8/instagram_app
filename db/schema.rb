dab92c7deaae3b5c9a3e92cb74970e624d27afdb
daiki8 <daiki1945815@gmail.com>
2018-10-03 22:14:24 +0900

    add posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180928072105) do
+ActiveRecord::Schema.define(version: 20181003130216) do
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "title"
+    t.string "picture"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email"
@@ -19,4 +29,5 @@ ActiveRecord::Schema.define(version: 20180928072105) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
