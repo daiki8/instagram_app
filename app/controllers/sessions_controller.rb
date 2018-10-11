6947dfe72b6738d007756defd53eb914c1a94aee
daiki8 <daiki1945815@gmail.com>
2018-10-11 13:49:30 +0900

    add alert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
           log_in user
+          flash[:success] = 'logged in!'
           redirect_to root_path
         else
           flash.now[:danger] = 'Invalid email/password combination'
