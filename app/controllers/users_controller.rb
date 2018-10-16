24c86bba35ade69c6e3cf48f0ce901aadce1be1f
daiki8 <daiki1945815@gmail.com>
2018-10-16 19:32:46 +0900

    update redirect_to login_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "created new user account!"
-      redirect_to signup_path
+      redirect_to login_path
     else
       render 'new'
     end
