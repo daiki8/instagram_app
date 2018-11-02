2b2bf5d685cb10bbbb193c805c2af8ce71d9a1d6
daiki8 <daiki1945815@gmail.com>
2018-11-02 20:46:37 +0900

    add remember

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
 	before_save :downcase_email
 	has_many :posts, dependent: :destroy
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\Z/i
@@ -8,6 +9,28 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true  # allow_nil:error-messageのダブりを防ぐため
 
+	# ランダムなトークンを返す
+	def User.new_token
+    SecureRandom.urlsafe_base64
+	end
+	
+	# 永続的セッションで使用するユーザーをデータベースに記憶する
+	def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	# ユーザーログインを破棄する
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
+	
+	# 渡されたトークンがダイジェストと一致したらtrueを返す
+	def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
 	private
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181003130216) do
+ActiveRecord::Schema.define(version: 20181102100136) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20181003130216) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
   add_foreign_key "posts", "users"
