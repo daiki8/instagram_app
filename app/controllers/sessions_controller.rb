1c4b46693a58f4535d22f7d60e20f2488f85f640
daiki8 <daiki1945815@gmail.com>
2018-11-15 00:44:06 +0900

    fix remember and forget

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-      user = User.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
-        log_in user
+      @user = User.find_by(email: params[:session][:email].downcase)
+      if @user && @user.authenticate(params[:session][:password])
+        log_in @user
         params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
         flash[:success] = 'logged in!'
         redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,22 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true  # allow_nil:error-messageのダブりを防ぐため
 
+
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+													BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+
 	# ランダムなトークンを返す
 	def User.new_token
-    SecureRandom.urlsafe_base64
+    	SecureRandom.urlsafe_base64
 	end
 	
 	# 永続的セッションで使用するユーザーをデータベースに記憶する
 	def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
 	end
 
 	# ユーザーログインを破棄する
@@ -27,10 +34,10 @@ class User < ApplicationRecord
 	
 	# 渡されたトークンがダイジェストと一致したらtrueを返す
 	def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
-    return false if digest.nil?
-    BCrypt::Password.new(digest).is_password?(token)
-  end
+		digest = send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
+  	end
 
 	private
   
